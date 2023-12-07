944cde4a00cc62d7bb76977963e17e82180b7b17
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-12-07 14:28:42 -0700

    Add after_sign_in_path_for method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || user_dashboard_index_path(current_user)
+    # method checks if there's a stored location (perhaps the user was trying to access a specific page before signing in). 
+    # If there's no stored location, it redirects to the dashboard_path.
+  end
 end
